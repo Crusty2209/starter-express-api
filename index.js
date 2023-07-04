acc94f3232a4b0c92c411fbd77894e48c4522aae
Crusty2209 <77834354+Crusty2209@users.noreply.github.com>
2023-07-04 21:40:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let balance = "0.00";
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.ugmarket.shop/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.ugmarket.shop/', 'https://ug-mkt-live.glitch.me/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });
