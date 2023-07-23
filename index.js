df121f8137ca6c23f24eca38b88dedd1f674aff8
Crusty2209 <77834354+Crusty2209@users.noreply.github.com>
2023-07-23 11:58:19 +1000

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 //https://www.ugmarket.shop
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://ugmarket.shop');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.ugmarket.shop');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
